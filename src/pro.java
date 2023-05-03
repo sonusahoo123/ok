2d34ad3a688254e388d66332ea8739e064a007d4
Sohanpsahoo <132354587+Sohanpsahoo@users.noreply.github.com>
2023-05-03 17:09:12 +0530

    Update pro.java

--- src/pro.java
+++ src/pro.java
@@ -12,7 +12,7 @@ public class AddTwoNumbers {
 
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
-        System.out.println("hii");
+        System.out.println("hii and hello");
         
     }
 }
