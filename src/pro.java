bab0c173edccb2a80bfd49551a2438a83c626f58
Sohan <sohanpriyadarshisahoo@gmail.com>
2023-05-03 16:57:57 +0530

    ok

--- src/pro.java
+++ src/pro.java
@@ -12,6 +12,6 @@ public class AddTwoNumbers {
 
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
-        System.out.println("Hes");
+        System.out.println("hii");
     }
 }
